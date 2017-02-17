a7658df4d6229b5fdc082e1508ca4219a0380e74
pkong0414 <patk0414@outlook.com>
2017-02-17 08:08:26 -0800

    added submit button and text area

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = ('Hello world!')
+        message = ("Hello World!")
         encrypted_message = caesar.encrypt(message, 13)
-        self.response.write(encrypted_message)
+        text_message = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type = 'submit'/>"
+        form = "<form>" + text_message + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
